3d862b23ed3c96d89187fa8d2bc486a2fcb4c07a
Jarrett Byrnes <jebyrnes@Jarretts-MacBook-Air-2.local>
2020-03-21 13:30:58 -0400

    updating with transformation.

--- scripts/get_county_shapefiles.R
+++ scripts/get_county_shapefiles.R
@@ -3,13 +3,14 @@ library(sf)
 setwd(here::here())
 get_county <- function(x){
   cat(paste0(x, " starting\n"))
-  acounty <- st_as_sf(counties(x, cb=TRUE))
+  acounty <- st_as_sf(counties(x, cb=TRUE))%>%
+    st_transform(crs = st_crs(us_map))
   cat(paste0(x, " saving\n"))
   saveRDS(acounty, file = paste0("./counties/", x, ".rds"))
   cat(paste0(x, " done\n"))
 }
 
-get_county("Massachusetts")
+#get_county("Massachusetts")
 
 
 us_map <- rnaturalearth::ne_states(country = "United States of America", returnclass = "sf")
